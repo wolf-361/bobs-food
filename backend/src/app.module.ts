cd4f58bffb5f898f764a184c245493c1f3b901b0
Luc Allaire <wolf-361@hotmail.com>
2024-04-02 11:15:15 -0400

    ajout config service et retirer erreur import

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientModule } from './client/client.module';
-import { EmployeModule } from './employe/employe.module';
+import { ClientModule } from './user/client/client.module';
+import { EmployeModule } from './user/employe/employe.module';
 import { CommandeModule } from './commande/commande.module';
 import { RestaurentModule } from './restaurent/restaurent.module';
 import { ItemModule } from './item/item.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [ClientModule, EmployeModule, CommandeModule, RestaurentModule, ItemModule],
+  imports: [ClientModule, EmployeModule, CommandeModule, RestaurentModule, ItemModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
