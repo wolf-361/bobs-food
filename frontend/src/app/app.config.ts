6160e17e32bae503520a2e2b30a87df428d63037
Luc Allaire <wolf-361@hotmail.ca>
2024-04-05 11:55:51 -0400

    add an interceptor to add the auth token to all requests

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,7 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { AuthInterceptor } from './interceptors/auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes), 
+    provideAnimationsAsync(),
+    // Add http interceptor to handle authorization
+    provideHttpClient(
+      withInterceptors([AuthInterceptor])
+    )
+  ]
 };
