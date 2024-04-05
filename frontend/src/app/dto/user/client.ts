b3b308f7beb2ccb8ec297d8d16f367c8d6e72512
Luc Allaire <wolf-361@hotmail.ca>
2024-04-05 11:07:20 -0400

    dto user

--- frontend/src/app/dto/user/client.ts
+++ frontend/src/app/dto/user/client.ts
@@ -1,2 +1,20 @@
-export class Client {
+import { User } from "./user";
+
+export class Client extends User {
+    courriel: string;
+    estInscrit: boolean;
+    titulaireCarteCredit: string;
+    numeroCarteCredit: string;
+    dateExpirationCarteCredit: string;
+    cvcCarteCredit: string;
+
+    constructor(id: string, nom: string, prenom: string, adresse: string, salt: string, hashedPassword: string, courriel: string, estInscrit: boolean, titulaireCarteCredit: string, numeroCarteCredit: string, dateExpirationCarteCredit: string, cvcCarteCredit: string) {
+        super(id, nom, prenom, adresse, salt, hashedPassword);
+        this.courriel = courriel;
+        this.estInscrit = estInscrit;
+        this.titulaireCarteCredit = titulaireCarteCredit;
+        this.numeroCarteCredit = numeroCarteCredit;
+        this.dateExpirationCarteCredit = dateExpirationCarteCredit;
+        this.cvcCarteCredit = cvcCarteCredit;
+    }
 }
