55f805e154c7bb885100298456ea6a47dfdee594
Luc Allaire <wolf-361@hotmail.ca>
2024-04-05 10:34:38 -0400

    add initial log

--- frontend/src/app/services/api/api.service.ts
+++ frontend/src/app/services/api/api.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { LoggerService } from '../logger/logger.service';
+import { HttpClient } from '@angular/common/http';
+import { environment as env } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private readonly apiUrl = env.apiUrl;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private logger: LoggerService
+  ) { 
+    this.logger.api('API service started');
+  }
+
+  
 }
