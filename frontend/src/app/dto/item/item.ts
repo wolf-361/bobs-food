5bf04fe112d20f596609096e7068a80efff7d6e6
Luc Allaire <wolf-361@hotmail.ca>
2024-04-05 10:48:20 -0400

    item et restaurent

--- frontend/src/app/dto/item/item.ts
+++ frontend/src/app/dto/item/item.ts
@@ -1,2 +1,19 @@
+import { ItemCategory } from "./item-categorie";
+
 export class Item {
+    id: number;
+    nom: string;
+    description: string;
+    prix: number;
+    image: string;
+    categorie: ItemCategory;
+
+    constructor(id: number, nom: string, description: string, prix: number, image: string, categorie: ItemCategory) {
+        this.id = id;
+        this.nom = nom;
+        this.description = description;
+        this.prix = prix;
+        this.image = image;
+        this.categorie = categorie;
+    }
 }
