5ebdc8cd88ceb16a514b69dca3f99f97d0e6b18b
Luc Allaire <wolf-361@hotmail.ca>
2024-04-06 12:31:11 -0400

    setup to have both client and employe login and sign up

--- frontend/src/app/pages/connexion/connexion.component.ts
+++ frontend/src/app/pages/connexion/connexion.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { SignupComponent } from './signup/signup.component';
 
 @Component({
   selector: 'app-connexion',
   standalone: true,
   imports: [
-    LoginComponent
+    LoginComponent,
+    SignupComponent,
+    MatTabsModule
   ],
   templateUrl: './connexion.component.html',
   styleUrl: './connexion.component.scss'
