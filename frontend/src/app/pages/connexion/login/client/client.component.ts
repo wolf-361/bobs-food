543d000c96df3979192acb9b5d1de6e790cfb0da
Luc Allaire <wolf-361@hotmail.ca>
2024-04-06 14:04:52 -0400

    start signup

--- frontend/src/app/pages/connexion/login/client/client.component.ts
+++ frontend/src/app/pages/connexion/login/client/client.component.ts
@@ -23,14 +23,15 @@ import { LoginResponse } from '../../../../services/auth/login.response';
     MatButtonModule,
     FormsModule,
     ReactiveFormsModule,
-    MatSnackBarModule],
+    MatSnackBarModule
+  ],
   templateUrl: './client.component.html',
   styleUrl: './client.component.scss'
 })
 export class ClientComponent {
   loginForm: FormGroup = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/), Validators.minLength(6), Validators.maxLength(20)]),
+    password: new FormControl('', [Validators.required]),
   });
 
   constructor(
--- frontend/src/app/pages/connexion/signup/client/client.component.ts
+++ frontend/src/app/pages/connexion/signup/client/client.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../../../../services/api/api.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../../services/auth/auth.service';
+import { LoginResponse } from '../../../../services/auth/login.response';
 
 @Component({
   selector: 'app-client',
   standalone: true,
   imports: [
+    CommonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
     FormsModule,
     ReactiveFormsModule,
+    MatSnackBarModule
   ],
   templateUrl: './client.component.html',
   styleUrl: './client.component.scss'
@@ -16,4 +33,66 @@ export class ClientComponent {
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/), Validators.minLength(6), Validators.maxLength(20)]),
   });
+
+  
+
+  constructor(
+    private api: ApiService,
+    private auth: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) { 
+    
+  }
+
+  /**
+ * Getter for easy access to form fields
+ * @returns {Object} The form controls
+ * @example form['email'].value
+ */
+  get form(): { [key: string]: AbstractControl } {
+    return this.signupForm.controls;
+  }
+
+
+  onSubmit() {
+    if (!this.signupForm.valid) {
+      return;
+    }
+
+    // TODO: signup
+  }
+
+  /**
+   * Handle the http success response from the signup request
+   * @param response The response to handle
+   */
+  private handleSignupSuccess(response: LoginResponse): void {
+    // Store the session in the auth service
+    this.auth.Session = response;
+
+    // Redirect to the home
+    this.router.navigate(['/']);   
+  }
+
+  /**
+   * Handle the http error response from the signup request
+   * @param error The error to handle
+   */
+  private handleSignupError(error: HttpErrorResponse) {
+    if (error.status === 401) {
+      this.snackBar.open('Email ou mot de passe invalide', 'Fermer', { duration: 5000 });
+      // Clear the fields
+      this.signupForm.reset();
+    }
+
+    if (error.status === 404) {
+      this.snackBar.open('Utilisateur introuvable', 'Fermer', { duration: 5000 });
+      this.signupForm.reset();
+    }
+    
+    if (error.status === 500) {
+      this.snackBar.open('Erreur interne du serveur', 'Fermer', { duration: 5000 });
+    }
+  }
 }
