8ee2e48c244da85191d840014456575740d79df5
Luc Allaire <wolf-361@hotmail.ca>
2024-04-06 12:31:11 -0400

    setup to have both client and employe login and sign up

---
+++ frontend/src/app/pages/connexion/login/client/client.component.ts
@@ -0,0 +1,98 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../../../../services/api/api.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../../services/auth/auth.service';
+import { LoginResponse } from '../../../../services/auth/login.response';
+
+@Component({
+  selector: 'app-client',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSnackBarModule],
+  templateUrl: './client.component.html',
+  styleUrl: './client.component.scss'
+})
+export class ClientComponent {
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/), Validators.minLength(6), Validators.maxLength(20)]),
+  });
+
+  constructor(
+    private api: ApiService,
+    private auth: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) { 
+    
+  }
+
+  /**
+ * Getter for easy access to form fields
+ * @returns {Object} The form controls
+ * @example form['email'].value
+ */
+  get form(): { [key: string]: AbstractControl } {
+    return this.loginForm.controls;
+  }
+
+
+  onSubmit() {
+    if (!this.loginForm.valid) {
+      return;
+    }
+
+    this.api.loginClient(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: (response) => this.handleLoginSuccess(response),
+      error: (error) => this.handleLoginError(error)
+    });
+  }
+
+  /**
+   * Handle the http success response from the login request
+   * @param response The response to handle
+   */
+  private handleLoginSuccess(response: LoginResponse): void {
+    // Store the session in the auth service
+    this.auth.Session = response;
+
+    // Redirect to the home
+    this.router.navigate(['/']);   
+  }
+
+  /**
+   * Handle the http error response from the login request
+   * @param error The error to handle
+   */
+  private handleLoginError(error: HttpErrorResponse) {
+    if (error.status === 401) {
+      this.snackBar.open('Email ou mot de passe invalide', 'Fermer', { duration: 5000 });
+      // Clear the fields
+      this.loginForm.reset();
+    }
+
+    if (error.status === 404) {
+      this.snackBar.open('Utilisateur introuvable', 'Fermer', { duration: 5000 });
+      this.loginForm.reset();
+    }
+    
+    if (error.status === 500) {
+      this.snackBar.open('Erreur interne du serveur', 'Fermer', { duration: 5000 });
+    }
+  }
+}
