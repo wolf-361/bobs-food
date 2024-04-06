09eb88e1e1ead9dd63ae1ddbc7cb2bb86406936f
Luc Allaire <wolf-361@hotmail.ca>
2024-04-06 13:03:32 -0400

    start setting up employe component

--- frontend/src/app/pages/connexion/signup/client/client.component.ts
+++ frontend/src/app/pages/connexion/signup/client/client.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-client',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './client.component.html',
   styleUrl: './client.component.scss'
 })
 export class ClientComponent {
-
+  signupForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/), Validators.minLength(6), Validators.maxLength(20)]),
+  });
 }
