7b6c0b703e9f468d33ec784e447d079de4f40451
Luc Allaire <wolf-361@hotmail.ca>
2024-04-05 21:50:31 -0400

    style login

--- frontend/src/app/pages/connexion/login/login.component.ts
+++ frontend/src/app/pages/connexion/login/login.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/), Validators.minLength(6), Validators.maxLength(20)]),
+  });
 
+  /**
+ * Getter for easy access to form fields
+ * @returns {Object} The form controls
+ * @example form['email'].value
+ */
+  get form(): { [key: string]: AbstractControl } {
+    return this.loginForm.controls;
+  }
+
+
+  onSubmit() {
+    if (!this.loginForm.valid) {
+      console.log('Invalid form');
+
+      return;
+    }
+  }
+
+  private handleLoginError(error: HttpErrorResponse) {
+    throw new Error('Method not implemented.');
+  }
 }
