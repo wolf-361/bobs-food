7f2e309d57327190230d43385107ad18c40617ba
Luc Allaire <wolf-361@hotmail.com>
2024-04-11 11:53:41 -0400

    prep pour julien

--- frontend/src/app/general/panier/panier.component.ts
+++ frontend/src/app/general/panier/panier.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { CommandeService } from '../../services/commande/commande.service';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 import { Router } from '@angular/router';
 import { Item } from '../../dto/item/item';
+import { ItemCategory } from '../../dto/item/item-categorie';
 
 @Component({
   selector: 'app-panier',
@@ -11,6 +13,7 @@ import { Item } from '../../dto/item/item';
   imports: [
     MatIconModule,
     MatButtonModule,
+    MatListModule,
   ],
   templateUrl: './panier.component.html',
   styleUrl: './panier.component.scss'
@@ -18,6 +21,17 @@ import { Item } from '../../dto/item/item';
 export class PanierComponent {
   isVide: boolean = true;
   items: { item: Item, quantite: number }[] = [];
+  categoryIcons: Map<ItemCategory, string> = new Map([
+    [ItemCategory.PIZZA, 'local_pizza'],
+    [ItemCategory.POUTINE, 'restaurant_menu'],
+    [ItemCategory.FRITE, 'fastfood'],
+    [ItemCategory.BURGER, 'hamburger'],
+    [ItemCategory.SANDWICH, 'sandwich'],
+    [ItemCategory.SALADE, 'salad'],
+    [ItemCategory.DESSERT, 'cake'],
+    [ItemCategory.BOISSON, 'local_drink'],
+    [ItemCategory.AUTRE, 'restaurant'],
+  ]);
 
   constructor(
     private commande: CommandeService,
@@ -27,6 +41,15 @@ export class PanierComponent {
     this.commande.Items.subscribe(items => this.isVide = items.length === 0);
     // Subscribe to the items in the command
     this.commande.Items.subscribe(items => this.items = items);
+  }
+
+  /**
+   * Get the icon to display for the given category
+   * @param category The category of the item
+   * @returns The string of the icon to display
+   */
+  getIcon(category: ItemCategory): string {
+    return this.categoryIcons.get(category) || 'restaurant';
   }
 
   /**
