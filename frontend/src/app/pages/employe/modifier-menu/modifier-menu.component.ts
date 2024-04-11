1001a9bd458e1ad3516ba73b3413e677aaa00434
Luc Allaire <wolf-361@hotmail.com>
2024-04-11 13:05:39 -0400

    début modifier menu

--- frontend/src/app/pages/employe/modifier-menu/modifier-menu.component.ts
+++ frontend/src/app/pages/employe/modifier-menu/modifier-menu.component.ts
@@ -1,12 +1,66 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Restaurent } from '../../../dto/restaurent/restaurent';
+import { ApiService } from '../../../services/api/api.service';
+import { Item } from '../../../dto/item/item';
+import { ItemComponent } from '../../../general/item/item.component';
 
 @Component({
   selector: 'app-modifier-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    ItemComponent
+  ],
   templateUrl: './modifier-menu.component.html',
   styleUrl: './modifier-menu.component.scss'
 })
 export class ModifierMenuComponent {
+  private _items!: BehaviorSubject<{ item: Item, isSelected: boolean }[]>;
+  restaurent!: Restaurent;
+  items!: { item: Item, isSelected: boolean }[];
 
+  constructor(
+    private api: ApiService,
+  ) {
+    // Initialize the restaurent and items
+    this._items = new BehaviorSubject<{ item: Item, isSelected: boolean }[]>([]);
+
+    // By default, get the first restaurent
+    api.getRestaurents().subscribe((restaurents) => {
+      this.restaurent = restaurents[0];
+
+
+      // Get the items to pick from
+      api.getItems().subscribe((items) => {
+        // If the items are in the selected restaurent, put them as selected
+        this._items.next(items.map((item) => {
+          return { item: item, isSelected: this.restaurent.menu.includes(item) };
+        }));
+      });
+    });
+
+    this._items.subscribe((items) => this.items = items);
+  }
+
+  // Toggle the selection of an item
+  toggleItemSelection(item: Item) {
+    // Update the items list
+    const items = this._items.getValue();
+    const index = items.findIndex((i) => i.item === item);
+    items[index].isSelected = !items[index].isSelected;
+    this._items.next(items);
+  }
+
+  // Save the changes
+  save() {
+    this.restaurent.menu = this._items.getValue().filter((i) => i.isSelected).map((i) => i.item);
+    this.api.patchRestaurent(this.restaurent.id, this.restaurent).subscribe({
+      next: (restaurent) => {
+        // Success
+      },
+      error: (error) => {
+        // Error
+      }
+    });
+  }
 }
