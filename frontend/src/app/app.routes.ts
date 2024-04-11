1b0277bc7298f9b3dc1074d2461eaf06a2d9d497
Luc Allaire <wolf-361@hotmail.com>
2024-04-10 20:38:18 -0400

    ajouter profile route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -10,6 +10,7 @@ import { CreationEmployeComponent } from './pages/employe/creation-employe/creat
 import { roleGuard } from './guards/role/role.guard';
 import { ModifierCommandeComponent } from './pages/employe/modifier-commande/modifier-commande.component';
 import { ModifierMenuComponent } from './pages/employe/modifier-menu/modifier-menu.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -20,12 +21,15 @@ export const routes: Routes = [
             { path: 'forgot-password', component: ConnexionComponent }
         ]
     },
+    { path: 'profile', component: ProfileComponent, canActivate: [roleGuard], data: { role: 'client, employe, gestionnaire, admin, proprietaire' } },
     { path: 'commander', component: CommandeComponent },
-    { path: 'employer', component: EmployeComponent, children: [
-        { path: 'create', component: CreationEmployeComponent, canActivate: [roleGuard], data: { role: 'gestionnaire, admin, proprietaire' } },
-        { path: 'modify-command', component: ModifierCommandeComponent, canActivate: [roleGuard], data: { role: 'employe'} },
-        { path: 'modify-menu', component: ModifierMenuComponent, canActivate: [roleGuard], data: { role: 'gestionnaire, admin, proprietaire'} },
-    ], canActivate: [roleGuard], data: { role: 'employe, gestionnaire, admin, proprietaire'}},
+    {
+        path: 'employer', component: EmployeComponent, children: [
+            { path: 'create', component: CreationEmployeComponent, canActivate: [roleGuard], data: { role: 'gestionnaire, admin, proprietaire' } },
+            { path: 'modify-command', component: ModifierCommandeComponent, canActivate: [roleGuard], data: { role: 'employe' } },
+            { path: 'modify-menu', component: ModifierMenuComponent, canActivate: [roleGuard], data: { role: 'gestionnaire, admin, proprietaire' } },
+        ], canActivate: [roleGuard], data: { role: 'employe, gestionnaire, admin, proprietaire' }
+    },
     { path: 'panier', component: PanierComponent },
     { path: '**', redirectTo: '' }
 ];
