76ca42b0acabc866ebba31cfb2f62be723e0401f
Luc Allaire <wolf-361@hotmail.ca>
2024-04-12 17:02:02 -0400

    prep restaurent service

--- frontend/src/app/services/restaurent/restaurent.service.ts
+++ frontend/src/app/services/restaurent/restaurent.service.ts
@@ -1,33 +1,48 @@
 import { Injectable } from '@angular/core';
 import { Restaurent } from '../../dto/restaurent/restaurent';
 import { ApiService } from '../api/api.service';
-import { map, Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurentService {
+  private prefix: string;
+  private _restaurent!: BehaviorSubject<Restaurent>;
 
   constructor(
-    private api: ApiService
-  ) { }
+    private api: ApiService,
+    private auth: AuthService
+  ) { 
+    this._restaurent = new BehaviorSubject<Restaurent>({} as Restaurent);
 
-  public set restaurent(restaurent: Restaurent) {
-    localStorage.setItem('restaurentId', restaurent.id);
+    if (this.restaurentId !== '') {
+      this.api.getRestaurent(this.restaurentId).subscribe((restaurent) => {
+        this._restaurent.next(restaurent);
+      });
+    } else {
+      this.api.getRestaurents().pipe(
+        map((restaurents) => restaurents[0])
+      ).subscribe((restaurent) => {
+        this._restaurent.next(restaurent);
+        this.restaurent = restaurent;
+      });
+    }
+    
+    this.prefix = this.auth.Role
   }
 
   public get restaurent(): Observable<Restaurent> {
-    if (!this.restaurentId) {
-      // Return the first restaurent in the list
-      return this.api.getRestaurents().pipe(
-        map(restaurents => restaurents[0])
-      );
-    } else {
-      return this.api.getRestaurent(this.restaurentId);
-    }
+    return this._restaurent.asObservable();
+  }
+
+  public set restaurent(restaurent: Restaurent) {
+    this._restaurent.next(restaurent); // Update the observable
+    localStorage.setItem(this.prefix + 'restaurentId', restaurent.id); // Update the local storage
   }
 
   private get restaurentId(): string {
-    return localStorage.getItem('restaurentId') || '';
+    return localStorage.getItem(this.prefix + 'restaurentId') || '';
   }
 }
