475ed9d9d1ddbceb2308b596d256765ee55e4268
Alexis Young <alexyoung@outlook.fr>
2024-04-11 20:46:45 -0400

    Début modif commande
    Ajout de la base du visuel

--- frontend/src/app/pages/employe/modifier-commande/modifier-commande.component.ts
+++ frontend/src/app/pages/employe/modifier-commande/modifier-commande.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle, MatCardSubtitle} from "@angular/material/card";
+import { Commande } from '../../../dto/commande/commande';
+import { ApiService } from '../../../services/api/api.service';
+import {NgForOf} from "@angular/common";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-modifier-commande',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardTitle,
+    MatCardContent,
+    MatCardActions,
+    NgForOf,
+    MatButton,
+    MatCardHeader,
+    MatCardSubtitle
+  ],
   templateUrl: './modifier-commande.component.html',
   styleUrl: './modifier-commande.component.scss'
 })
 export class ModifierCommandeComponent {
 
+  // Commandes list from API
+  commandes: Commande[] = [];
+
+
+  constructor(
+    private api: ApiService
+  ) {
+    this.api.getCommandes().subscribe((commandes: Commande[]) => {
+      this.commandes = commandes;
+    });
+  }
+
+  onEditCommande() {
+
+  }
+
+  onDeleteCommande() {
+
+  }
 }
