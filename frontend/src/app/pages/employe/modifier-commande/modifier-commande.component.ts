3d73fb614714439b444e24fde5dfa110ca47d042
Luc Allaire <wolf-361@hotmail.ca>
2024-04-14 22:14:07 -0400

    style modifier commande

--- frontend/src/app/pages/employe/modifier-commande/modifier-commande.component.ts
+++ frontend/src/app/pages/employe/modifier-commande/modifier-commande.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle, MatCardSubtitle} from "@angular/material/card";
 import { Commande } from '../../../dto/commande/commande';
 import { ApiService } from '../../../services/api/api.service';
-import {NgForOf} from "@angular/common";
+import {DatePipe, NgForOf} from "@angular/common";
 import {MatButton} from "@angular/material/button";
 import {Observable} from "rxjs";
 
@@ -17,15 +17,13 @@ import {Observable} from "rxjs";
     NgForOf,
     MatButton,
     MatCardHeader,
-    MatCardSubtitle
+    MatCardSubtitle,
+    DatePipe
   ],
   templateUrl: './modifier-commande.component.html',
   styleUrl: './modifier-commande.component.scss'
 })
 export class ModifierCommandeComponent {
-
-
-
   commandes: Commande[] = [];
 
   constructor(private api: ApiService) {
