3bb884122acdecddb72c2f8d2d9c3ed6088e87f8
Luc Allaire <wolf-361@hotmail.ca>
2024-04-28 16:50:08 -0400

    add buttons

--- frontend/src/app/overlays/item-popup/item-popup.component.ts
+++ frontend/src/app/overlays/item-popup/item-popup.component.ts
@@ -5,6 +5,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CommandeService } from '../../services/commande/commande.service';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { BehaviorSubject } from 'rxjs';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-item-popup',
@@ -20,12 +22,34 @@ import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/materia
 export class ItemPopupComponent {
   item: Item;
   isDansCommande: boolean = false;
+  private mouseOver: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  isMouseOver: boolean = false;
+  isMobile: boolean = false;
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.mouseOver.next(true);
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.mouseOver.next(false);
+  }
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: { item: Item },
     private dialogRef: MatDialogRef<ItemPopupComponent>,
-    private command: CommandeService
+    private command: CommandeService,
+    private breakpointObserver: BreakpointObserver,
   ) {
+    // Check if the mouse is over the item
+    this.mouseOver.subscribe((isMouseOver: boolean) => this.isMouseOver = isMouseOver);
+
+    // Listen to the screen size
+    this.breakpointObserver.observe('(max-width: 600px)').subscribe(result => {
+      this.isMobile = result.matches;
+    });
+
     this.item = data.item;
     this.command.Items.subscribe(items => {
       this.isDansCommande = items.some(i => i.item.id === this.item.id);
@@ -33,6 +57,8 @@ export class ItemPopupComponent {
   }
 
   addItem() {
+    console.log('Adding item to cart');
+    
     this.command.addItem(this.item);
   }
 
